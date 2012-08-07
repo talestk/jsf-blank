3f50f5d8e6761df6d0e06ccdb6d4ba7fcf618a4e
Tales Takemiya <talestk@hotmail.com>
2012-08-07 18:12:33 -0500

    Adding project on windows!

--- src/controller/JsoupHtmlParser.java
+++ src/controller/JsoupHtmlParser.java
@@ -4,12 +4,7 @@
  */
 package controller;
 
-import apple.laf.JRSUIState;
 import java.io.IOException;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 /**
  *
